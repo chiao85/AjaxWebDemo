c3b8b87cf568a5f812d8a4727e7b9c608b6d41a4
chiao85 <chiaoqaz@gmail.com>
2023-01-06 10:03:15 +0800

    套件

---
+++ prjWebDemo/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace prjWebDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Content", "text/plain", Encoding.UTF8);
+        }
+        public ActionResult TestAjax()
+        {
+            return View();
+        }
+    }
+}
