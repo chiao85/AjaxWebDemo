857797226d45ae95768ab2c57eb4bec0488de8d6
chiao85 <chiaoqaz@gmail.com>
2023-01-06 23:13:35 +0800

    桃園觀光景點作業

---
+++ prjWebDemo/Controllers/HomeworkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjWebDemo.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
