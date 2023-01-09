be5ed4fcebab3bd975dc2be8a2c86fcd9375ab8e
chiao85 <chiaoqaz@gmail.com>
2023-01-09 23:16:01 +0800

    作業三

--- prjWebDemo/Controllers/HomeworkController.cs
+++ prjWebDemo/Controllers/HomeworkController.cs
@@ -19,13 +19,36 @@ namespace prjWebDemo.Controllers
                 return Content("此會員名稱已被註冊!", "text/plain", Encoding.UTF8);
             return Content($"註冊成功!歡迎新會員:{Name}", "text/plain", Encoding.UTF8);
         }
-        public IActionResult List()
-        {
-            return View();
-        }
-        public ActionResult signUp()
-        {
-            return View();
-        }
-    }
+       
+		public ActionResult city()
+		{
+			var cityName = _context.Addresses.Select(c => c.City).Distinct();
+
+			return Json(cityName);
+		}
+		//以城市名稱讀取鄉鎮
+		public ActionResult site(string city)
+		{
+			var site = _context.Addresses.Where(c => c.City == city).Select(s => s.SiteId).Distinct();
+			return Json(site);
+		}
+		//以鄉鎮名稱讀取路名
+		public ActionResult road(string site)
+		{
+			var road = _context.Addresses.Where(s => s.SiteId == site).Select(r => r.Road).Distinct();
+			return Json(road);
+		}
+		public IActionResult List()
+		{
+			return View();
+		}
+		public ActionResult signUp()
+		{
+			return View();
+		}
+		public ActionResult fetch()
+		{
+			return View();
+		}
+	}
 }
